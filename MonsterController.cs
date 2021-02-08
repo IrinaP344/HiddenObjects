2b351656e17e3ff069fdb3333293d82f863a210c
IrinaP344 <71124960+IrinaP344@users.noreply.github.com>
2021-02-08 15:39:07 +0900

    Add files via upload

---
+++ MonsterController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MonsterController : MonoBehaviour
+{
+	Transform monster_transform;
+	public GameObject mon;
+	Transform mon_transform;
+	public GameObject mones;
+	Transform mones_transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        monster_transform = GetComponent<Transform>();
+        mon_transform = mon.GetComponent<Transform>();
+        mones_transform = mones.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+    	monster_transform.Translate(0,0,4f);
+    	mon_transform.Translate(0,0,4f);
+    	mones_transform.Translate(0,0,4f);
+    	SceneManager.LoadScene("scene5");
+    	
+    }
+}
---
+++ ThingsController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThingsController : MonoBehaviour
+{
+	Transform thing_transform;
+	public GameObject text;
+	Transform text_transform;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        thing_transform = GetComponent<Transform>();
+        text_transform = text.GetComponent<Transform>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+    	Destroy(text);
+    	
+    }
+}
